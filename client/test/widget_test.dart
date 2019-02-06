0d9925879aabd42f3c29c78434bbc149938c7a6b
jonnylin13 <jonnylin13@gmail.com>
2019-02-05 18:55:37 -0800

    added bottom navigation

--- client/test/widget_test.dart
+++ client/test/widget_test.dart
@@ -11,12 +11,12 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:client/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Navigation smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(TasteTheWaste());
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
+    expect(find.text('Home'), findsOneWidget);
     expect(find.text('1'), findsNothing);
 
     // Tap the '+' icon and trigger a frame.
