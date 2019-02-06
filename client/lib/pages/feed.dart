3b5ed1bf7ae41b54fbcb5ad377cbd4ef69948bae
jonnylin13 <jonnylin13@gmail.com>
2019-02-05 19:08:27 -0800

    cleanup

---
+++ client/lib/pages/feed.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Feed extends StatefulWidget {
+  Feed({Key key}) : super(key: key);
+
+  @override
+  _FeedState createState() => _FeedState();
+}
+
+class _FeedState extends State<Feed> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Feed');
+  }
+}
---
+++ client/lib/pages/settings.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Settings extends StatefulWidget {
+  Settings({Key key}) : super(key: key);
+
+  @override
+  _SettingsState createState() => _SettingsState();
+}
+
+class _SettingsState extends State<Settings> {
+  @override
+  Widget build(BuildContext context) {
+    return Text('Settings');
+  }
+}
