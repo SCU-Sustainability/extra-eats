48e17970ba6fddc4c17e4dcfc81c38d7e6d8682e
jonnylin13 <jonnylin13@gmail.com>
2019-02-13 00:02:31 -0800

    image upload working

---
+++ server/src/multer/upload.js
@@ -0,0 +1,36 @@
+const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
+
+
+aws.config.update(
+  {
+    secretAccessKey: process.env.AWS_ACCESS_KEY,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+    region: 'us-west-2'
+  }
+);
+const s3 = new aws.S3();
+
+const upload = multer({
+  storage: multerS3({
+    s3: s3,
+    bucket: 'elasticbeanstalk-us-west-2-790161425636',
+    acl: 'public-read',
+    metadata: function (req, file, callback) {
+      callback(null, Object.assign({}, req.body));
+    },
+    key: function (req, file, callback) {
+      callback(null, Date.now().toString() + '.jpg');
+    }
+  }), fileFilter: function(req, file, callback) {
+    console.log(req.body);
+    if (!req.body.description || !req.body.name) {
+      callback(null, false);
+      return;
+    }
+    callback(null, true);
+  }
+});
+
+module.exports = upload;
