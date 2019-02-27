4bd8bdb6a448673bae4a410a1b701a0610837d24
jonnylin13 <jonnylin13@gmail.com>
2019-02-27 03:19:36 -0800

    prettier with dart

--- client/lib/actions.dart
+++ client/lib/actions.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
 class InheritedClient extends InheritedWidget {
-
-  InheritedClient({Key key, @required this.login, 
-    @required this.logout, 
-    @required this.register,
-    @required this.accessToken,
-    @required this.userId,
-    @required Widget child}): 
-    assert(login != null),
-    assert(logout != null),
-    super(key: key, child: child);
+  InheritedClient(
+      {Key key,
+      @required this.login,
+      @required this.logout,
+      @required this.register,
+      @required this.accessToken,
+      @required this.userId,
+      @required Widget child})
+      : assert(login != null),
+        assert(logout != null),
+        super(key: key, child: child);
 
   final VoidCallback logout;
   final Function(String, String) login;
@@ -24,22 +25,26 @@ class InheritedClient extends InheritedWidget {
 
   @override
   bool updateShouldNotify(InheritedClient old) {
-    return (old.login != login) && (old.logout != logout) && (old.register != register)
-      && (old.accessToken != accessToken) && (old.userId != userId);
+    return (old.login != login) &&
+        (old.logout != logout) &&
+        (old.register != register) &&
+        (old.accessToken != accessToken) &&
+        (old.userId != userId);
   }
 }
 
 // Deprecated
 class GoogleLoginAction extends InheritedWidget {
-  GoogleLoginAction({Key key, @required this.login, @required Widget child}): assert(login != null), 
-    assert(child != null),
-    super(key: key, child: child);
+  GoogleLoginAction({Key key, @required this.login, @required Widget child})
+      : assert(login != null),
+        assert(child != null),
+        super(key: key, child: child);
   final VoidCallback login;
 
   static GoogleLoginAction of(BuildContext context) {
     return context.inheritFromWidgetOfExactType(GoogleLoginAction);
   }
-  
+
   @override
   bool updateShouldNotify(GoogleLoginAction old) => login != old.login;
 }
