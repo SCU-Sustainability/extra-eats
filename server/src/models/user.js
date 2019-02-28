5f0c9a840daae684aaf041509d873fe32c424636
jonnylin13 <jonnylin13@gmail.com>
2019-02-27 22:23:20 -0800

    work on #53

--- server/src/models/user.js
+++ server/src/models/user.js
@@ -5,36 +5,36 @@ var Schema = mongoose.Schema;
 var ObjectId = mongoose.Schema.Types.ObjectId;
 
 var UserSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
-    unique: false
-  },
-  password: {
-    type: String,
-    required: true,
-    unique: false,
-    select: false
-  },
-  email: {
-    type: String,
-    required: true,
-    unique: true
-  },
-  emailToken: {
-    type: String,
-    required: true,
-    unique: false
-  },
-  posts: {
-    type: [ObjectId],
-    required: true,
-    unique: false
-  },
-  provider: {
-    type: Boolean,
-    required: true
-  }
+	name: {
+		type: String,
+		required: false,
+		unique: false
+	},
+	password: {
+		type: String,
+		required: true,
+		unique: false,
+		select: false
+	},
+	email: {
+		type: String,
+		required: true,
+		unique: true
+	},
+	emailToken: {
+		type: String,
+		required: true,
+		unique: false
+	},
+	posts: {
+		type: [ObjectId],
+		required: true,
+		unique: false
+	},
+	provider: {
+		type: Boolean,
+		required: true
+	}
 });
 
 module.exports = mongoose.model("User", UserSchema);
