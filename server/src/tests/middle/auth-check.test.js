4b7fa7aa0f70c85d11e65b8efe6c06d7e2ca1214
jonnylin13 <jonnylin13@gmail.com>
2019-03-05 12:28:50 -0800

    finished #57 and #58

---
+++ server/src/tests/middle/auth-check.test.js
@@ -0,0 +1,64 @@
+const expect = require('chai').expect;
+const authCheck = require('../../middle/auth-check');
+
+let req = {
+  headers: {
+    'x-access-token': 'test'
+  },
+  body: {},
+  path: 'api/users',
+  method: 'GET'
+};
+
+let req2 = {
+  headers: {},
+  body: {},
+  path: 'api/users',
+  method: 'GET'
+};
+
+let req3 = {
+  headers: {},
+  body: {},
+  path: 'api/users',
+  method: 'POST'
+};
+
+let res = {
+  code: null,
+  json: function(arg) {
+    this.code = arg.code;
+  }
+};
+
+const authRoutes = ['users'];
+const exclusions = {
+  users: 'POST'
+};
+
+describe('Auth check middleware', function() {
+  describe('Auth check with token', function() {
+    it('Should call next()', function() {
+      let next = false;
+      authCheck(authRoutes, exclusions, req, res, () => {
+        next = true;
+      });
+      expect(next).to.equal(true);
+    });
+  });
+  describe('Auth check without token', function() {
+    it('Should return code -99', function() {
+      authCheck(authRoutes, exclusions, req2, res, () => {});
+      expect(res.code).to.equal(-99);
+    });
+  });
+  describe('Auth check without token to exclusion', function() {
+    it('Should call next()', function() {
+      let next = false;
+      authCheck(authRoutes, exclusions, req, res, () => {
+        next = true;
+      });
+      expect(next).to.equal(true);
+    });
+  });
+});
