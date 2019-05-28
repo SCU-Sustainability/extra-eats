0620001943dbfb31819e62cf86b94a8edb7605a1
Sinclair Chen <abc.sinclair@gmail.com>
2019-05-27 18:31:26 -0700

    Added stayLogin setting. Settings can now save.

--- client/lib/pages/settings.dart
+++ client/lib/pages/settings.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import '../actions.dart';
 
@@ -10,50 +11,129 @@ class Settings extends StatefulWidget {
 }
 
 class _SettingsState extends State<Settings> {
-  bool _pushNotifications = true;
+  var settings = {
+    'notifications': true,
+    'stayLogin': true,
+  };
+  bool _saved = true;
+
+  Future savePreferences() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    prefs.setBool('notifications', settings['notifications']);
+    prefs.setBool('stayLogin', settings['stayLogin']);
+    setState(() {
+      _saved = true;
+    });
+    return true;
+  }
+
+  Future getPreferences() async {
+    final SharedPreferences prefs = await SharedPreferences.getInstance();
+    settings['notifications'] = prefs.getBool('notifications') ?? true;
+    settings['stayLogin'] = prefs.getBool('stayLogin') ?? true;
+    _saved = true;
+    return settings;
+  }
+
+  Widget settingsSwitch(BuildContext context,
+      {Icon icon, String label, String setting}) {
+    return ListTile(
+        leading: icon,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            Text(label, style: TextStyle(fontWeight: FontWeight.w300)),
+            Spacer(flex: 1),
+            Transform.scale(
+                scale: 1.5,
+                child: Switch(
+                  value: settings[setting],
+                  onChanged: (bool changed) async {
+                    setState(() {
+                      settings[setting] = changed;
+                      _saved = false;
+                    });
+                  },
+                ))
+          ],
+        ),
+        onTap: () async {
+          setState(() {
+            settings[setting] = !settings[setting];
+            _saved = false;
+          });
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
-    return ListView(physics: NeverScrollableScrollPhysics(), children: <Widget>[
-      ListTile(
-          leading: Icon(Icons.notifications),
-          title: Row(
-            mainAxisAlignment: MainAxisAlignment.start,
-            children: <Widget>[
-              Text('Push notifications: ',
+    var errorStyle = TextStyle(color: Theme.of(context).errorColor);
+    if (!_saved)
+      return settingsList(context);
+    else
+      return FutureBuilder(
+          future: getPreferences(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            switch (snapshot.connectionState) {
+              case ConnectionState.none:
+                return Text('Can\'t connect', style: errorStyle);
+              case ConnectionState.active:
+              case ConnectionState.waiting:
+                return CircularProgressIndicator();
+              case ConnectionState.done:
+                if (snapshot.hasError)
+                  return Text('Error: ${snapshot.error}', style: errorStyle);
+                //else
+                return settingsList(context);
+            }
+          });
+  }
+
+  Widget settingsList(BuildContext context) {
+    return ListView(
+        physics: NeverScrollableScrollPhysics(),
+        shrinkWrap: true,
+        children: <Widget>[
+          settingsSwitch(context,
+              icon: Icon(Icons.person),
+              label: 'Stay logged in',
+              setting: 'stayLogin'),
+          settingsSwitch(context,
+              icon: Icon(Icons.notifications),
+              label: 'Push notifications',
+              setting: 'notifications'),
+          ListTile(
+              leading: Icon(Icons.fastfood),
+              title: Text('Hygiene guidelines',
                   style: TextStyle(fontWeight: FontWeight.w300)),
-              Spacer(flex: 1),
-              Transform.scale(
-                  scale: 1.5,
-                  child: Switch(
-                    value: _pushNotifications,
-                    onChanged: (bool changed) {
-                      setState(() {
-                        _pushNotifications = changed;
-                      });
-                    },
-                  )),
-            ],
+              onTap: () {} //nothing yet
+              ),
+          ListTile(
+              leading: Icon(Icons.assignment),
+              title:
+                  Text('Legal', style: TextStyle(fontWeight: FontWeight.w300)),
+              onTap: () {} //nothing yet
+              ),
+          ListTile(
+            leading: Icon(Icons.exit_to_app),
+            title:
+                Text('Sign out', style: TextStyle(fontWeight: FontWeight.w300)),
+            onTap: () async {
+              settings['stayLogin'] = false;
+              await savePreferences();
+              InheritedClient.of(context).logout();
+            },
           ),
-          onTap: () => setState(() {
-                _pushNotifications = !_pushNotifications;
-              })),
-      ListTile(
-          leading: Icon(Icons.fastfood),
-          title: Text('Hygiene guidelines',
-              style: TextStyle(fontWeight: FontWeight.w300)),
-          onTap: (){} //nothing yet
-          ),
-      ListTile(
-          leading: Icon(Icons.assignment),
-          title: Text('Legal', style: TextStyle(fontWeight: FontWeight.w300)),
-          onTap: (){} //nothing yet
-          ),
-      ListTile(
-        leading: Icon(Icons.exit_to_app),
-        title: Text('Sign out', style: TextStyle(fontWeight: FontWeight.w300)),
-        onTap: InheritedClient.of(context).logout,
-      ),
-    ]);
+          if (!_saved)
+            RaisedButton(
+                textColor: Colors.white,
+                child: Text('Save'),
+                onPressed: savePreferences)
+          //Text('Sponsored by', style: TextStyle(fontWeight: FontWeight.w300)),
+          // Image(
+          //     image: AssetImage('assets/sambazon.png'),
+          //     width: 200.0,
+          //     height: 200.0)
+        ]);
   }
 }
