08bb3b5133dcddc6400cd214db566dc3cda3c320
Sinclair Chen <abc.sinclair@gmail.com>
2019-05-28 21:15:39 -0700

    Matt's Changes

--- server/src/upload.js
+++ server/src/upload.js
@@ -3,18 +3,20 @@ const cloudinary = require('cloudinary');
 const cloudinaryStorage = require('multer-storage-cloudinary');
 
 cloudinary.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.CLOUD_API_SECRET
+  cloud_name: process.env.MY_CLOUD_NAME,
+  api_key: process.env.MY_CLOUD_API_KEY,
+  api_secret: process.env.MY_CLOUD_API_SECRET
 });
 
-const storage = cloudinaryStorage({
+var storage = cloudinaryStorage({
   cloudinary: cloudinary,
   folder: 'ttw',
-  allowedFormats: ['jpg', 'jpeg', 'png'],
-  transformation: [{ width: 500, height: 500, crop: 'limit' }]
+  allowedFormats: ['jpg', 'png'],
+  filename: function (req, file, cb) {
+    cb(undefined, file.originalname);
+  }
 });
 
-const parser = multer({ storage: storage });
+const parser = multer({storage: storage});
 
 module.exports = parser;
