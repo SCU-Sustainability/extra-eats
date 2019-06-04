896aa5574b5ca83597b12c1b816cf07cac3247e7
MattyJ10 <jasaitismatt@yahoo.com>
2019-06-03 21:24:01 -0700

    removed print statements

--- server/src/background.js
+++ server/src/background.js
@@ -13,13 +13,13 @@ exports.updatePostStatus = function() {
 		var dayOfWeek = rightNow.getDay(); 
 		var numberDay = rightNow.getDate(); 
 		var month = rightNow.getMonth() + 1; 
-		console.log(hour);
-		console.log(minutes); 
-		console.log(numberDay);
-		console.log(month);
+		// console.log(hour);
+		// console.log(minutes); 
+		// console.log(numberDay);
+		// console.log(month);
 
 		post.updateMany({scheduledDay: numberDay, scheduledMonth: month, scheduledMinute: minutes, scheduledHour: hour}, {$set: {status: 'active'}}).exec(function(err, posts) {
-			console.log(posts); 
+			console.log("Posts made active: ", posts.nModified); 
 		})
 
 	}) 
